6734eaf269e75afbcade2ed77e94337e848bd73e
Goshawk <goshawk@Goshawks-Mac.local>
2019-04-29 17:14:19 +0800

    made a design with socail media

---
+++ Homepagedesign/HomepagedesignTests/HomepagedesignTests.swift
@@ -0,0 +1,34 @@
+//
+//  HomepagedesignTests.swift
+//  HomepagedesignTests
+//
+//  Created by Goshawk on 4/29/19.
+//  Copyright © 2019 Goshawk. All rights reserved.
+//
+
+import XCTest
+@testable import Homepagedesign
+
+class HomepagedesignTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
